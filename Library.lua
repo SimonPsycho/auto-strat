75245302599807760a9a68930d94e1bf43eb40c2
SimonPsycho <118612402+SimonPsycho@users.noreply.github.com>
2026-01-03 15:07:59 -0300

    Add files via upload

--- Library.lua
+++ Library.lua
@@ -1,17 +1,16 @@
-print("Library carregada com sucesso")
--- auto-strat Library
+print("Library carregada")
+
 local TDS = {}
 TDS.__index = TDS
 
 function TDS.new()
+    print("Instância criada")
     return setmetatable({}, TDS)
 end
 
-function TDS.new()
-    print("TDS.new() chamado")
-    return setmetatable({}, TDS)
+function TDS:Test()
+    print("Test OK")
 end
-
 
 return function()
     return TDS.new()
